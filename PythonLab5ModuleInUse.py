50a4a9ff5025bcad7af362e4b8ba3599096f588c
Kirill <kirilldanilovski@yandex.ru>
2022-06-10 19:37:39 +0300

    Практическое занятие 5 Добавил модули по заданию

--- PythonLab5ModuleInUse.py
+++ PythonLab5ModuleInUse.py
@@ -1,10 +1,11 @@
 import PythonLab5Module
 
 print('Введите имя первого игрока')
-igrok1 = PythonLab5Module.enterName()
+igrok1 = PythonLab5Module.enter_name()
 print('Введите имя второго игрока')
-igrok2 = PythonLab5Module.enterName()
+igrok2 = PythonLab5Module.enter_name()
 
 score1 = 0
 score2 = 0
-PythonLab5Module.resultGame(PythonLab5Module.thrGame(igrok1, score1), PythonLab5Module.thrGame(igrok2, score2), igrok1, igrok2)
+PythonLab5Module.result_game(PythonLab5Module.thr_game(igrok1, score1),
+                            PythonLab5Module.thr_game(igrok2, score2), igrok1, igrok2)
